c74f214e9fda25cf0b72d03e1dbb075b4f7c1611
damhs <ihansaem1@kaist.ac.kr>
2025-01-17 17:43:48 +0900

    BE init

---
+++ index.js
@@ -0,0 +1,32 @@
+//index.js
+const express = require("express");
+const http = require("http");
+const webSocketServer = require("websocket").server;
+
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+var server = http.createServer(app).listen(port, function () {
+  console.log("Express server listening");
+});
+
+var wsServer = new webSocketServer({
+  httpServer: server,
+});
+
+var eventHandler = {
+  handleRequest: function (request) {
+    console.log("Connection from origin " + request.origin + ".");
+  },
+  handleMessage: function (message) {
+    console.log(JSON.stringify(message));
+  },
+  handleClose: function (connection) {
+    console.log("disconnected.");
+  },
+};
+wsServer.on("request", eventHandler.handleRequest);
